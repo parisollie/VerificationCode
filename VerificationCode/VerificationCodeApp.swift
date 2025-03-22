afe8b9f68c45725120359d85461806b88a184fbe
parisollie <pjff18@gmail.com>
2025-03-22 13:39:15 -0600

    Initial Commit

---
+++ VerificationCode/VerificationCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerificationCodeApp.swift
+//  VerificationCode
+//
+//  Created by Paul F on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct VerificationCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
